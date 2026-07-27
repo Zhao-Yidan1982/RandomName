414700acf0e74011144000bb4df9e562ff7acaa1
Zhao-Yidan1982 <18210967602@163.com>
2026-07-27 21:23:26 +0800

    modified:   main.py 	modified:   models/__init__.py

--- main.py
+++ main.py
@@ -40,16 +40,16 @@ for enc in encodings:
         with open(f"config/{filename}.json", "r", encoding="utf-8") as f:
             settings = json.load(f)
     except FileNotFoundError:
-        print_wrong('配置文件不存在')
+        print_error('配置文件不存在')
         input("请按回车键退出程序")
         exit()
     except PermissionError:
-        print_wrong('无读取配置文件权限不足')
+        print_error('无读取配置文件权限不足')
         input("请检查文件权限并按回车键退出程序")
         exit()
     except UnicodeDecodeError:
         if enc == encodings[-1]:  # 已经是最后一种编码
-            print_wrong('不支持此编码的配置文件')
+            print_error('不支持此编码的配置文件')
             input("请使用GBK或UTF-8\n并按回车键退出运行")
             exit()
 
@@ -60,16 +60,16 @@ for enc in encodings:
             mnls_main = nmls.readlines()
         break  # 读取成功，跳出循环
     except FileNotFoundError:
-        print_wrong('数据库文件不存在')
+        print_error('数据库文件不存在')
         input("请按回车键退出程序")
         exit()
     except PermissionError:
-        print_wrong('无读取数据库文件权限不足')
+        print_error('无读取数据库文件权限不足')
         input("请检查文件权限并按回车键退出程序")
         exit()
     except UnicodeDecodeError:
         if enc == encodings[-1]:  # 已经是最后一种编码
-            print_wrong('不支持此编码的数据库文本')
+            print_error('不支持此编码的数据库文本')
             input("请使用GBK或UTF-8\n并按回车键退出运行")
             exit()
 
@@ -91,7 +91,7 @@ if settings['IncludeSex']:
     for i in tmp_list[settings['RowStart']:settings['RowEnd']]:
         if len(i) <= settings['SexColumn'] or \
         "男" in i[settings['SexColumn']] == "女" in i[settings['SexColumn']]:
-            print_wrong("数据库性别设置不正确")
+            print_error("数据库性别设置不正确")
             input("请检查数据库性别列并按回车键退出运行")
             exit()
     for i in tmp_list[settings['RowStart']:settings['RowEnd']]:
@@ -102,14 +102,14 @@ if settings['IncludeSex']:
             female_name.append(i[settings['NameColumn']])
             female_number.append(i[settings['NumberColumn']])
     if not male_name + female_name:
-        print_wrong("数据库为空")
+        print_error("数据库为空")
         input('请按回车键退出运行')
         exit()
     elif not male_name:
-        print_error("无男性数据")
+        print_warning("无男性数据")
         only_one_sex = True
     elif not female_name:
-        print_error("无女性数据")
+        print_warning("无女性数据")
         only_one_sex = True
 else:
     num_list = []
@@ -123,7 +123,7 @@ else:
 if settings['IncludeWeight']:
     for i in tmp_list[settings['RowStart']:settings['RowEnd']]:
         if len(i) <= settings['WeightColumn'] and is_integer(i[settings['WeightColumn']]):
-            print_wrong("数据库权重列设置不正确")
+            print_error("数据库权重列设置不正确")
             input("请检查数据库权重列并按回车键退出运行")
             exit()
 
@@ -173,7 +173,7 @@ while True:
             multiple_number_pick(num_list)
     elif mode == "5":
         if only_one_sex:
-            print_wrong("仅单性别数据无法使用此模式")
+            print_error("仅单性别数据无法使用此模式")
         else:
             single_gender_select_name_pick(male_name,female_name)
     elif mode == "6":
@@ -189,5 +189,5 @@ while True:
     elif mode == "exit":
         exit()
     else:
-        print_error('未识别的模式编号')
+        print_warning('未识别的模式编号')
         print('请重新选择模式')
--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,10 @@
 import random
 
 #定义错误提示函数
+def print_warning(message):
+    print(f"Warning:{message}!!\n")
 def print_error(message):
-    print(f"error:{message}!!\n")
-def print_wrong(message):
-    print(f"wrong:{message}!!!\n")
+    print(f"Error:{message}!!!\n")
 
 #定义1.单人姓名抽取函数
 def single_name_pick(all_name:list):
@@ -32,18 +32,18 @@ def multiple_name_pick(name_list:list):
         try:
             num = int(mode_input)
         except ValueError:
-            print_error('请输入整数')
+            print_warning('请输入整数')
             continue
         print()
         if num <= 0:
-            print_error("请输入正整数")
+            print_warning("请输入正整数")
         elif num <= len(name_list):
             random.shuffle(name_list)
             for i in name_list[:num]:
                 print(i,end=" ")
             print("\n")
         else:
-            print_error(f"姓名数量不足,仅{len(name_list)}个,无法抽取")
+            print_warning(f"姓名数量不足,仅{len(name_list)}个,无法抽取")
             print('\n')
 
 #定义3.单人编号抽取函数
@@ -72,18 +72,18 @@ def multiple_number_pick(num_list:list):
         try:
             num = int(num)
         except ValueError:
-            print_error('请输入整数')
+            print_warning('请输入整数')
             continue
         print()
         if num <= 0:
-            print_error("请输入正整数")
+            print_warning("请输入正整数")
         elif num <= len(num_list):
             random.shuffle(num_list)
             for i in num_list[:num]:
                 print(i,end=" ")
             print("\n")
         else:
-            print_error(f"编号数量不足,仅{len(num_list)}个,无法抽取")
+            print_warning(f"编号数量不足,仅{len(num_list)}个,无法抽取")
             print('\n')
 
 #定义5.单人性别选择姓名抽取函数
@@ -100,13 +100,13 @@ def single_gender_select_name_pick(male_name:list, female_name:list):
                 print(random.choice(male_name))
                 print()
             else:
-                print_error("无男性数据")
+                print_warning("无男性数据")
         elif sex_choice == "female":
             if female_name:
                 print(random.choice(female_name))
                 print()
             else:
-                print_error("无女性数据")
+                print_warning("无女性数据")
         else:
             print(random.choice(male_name + female_name))
             print()
